cc74831b2c638bbd992b13af7d2e19f7915ea1fb
Kazuki Kuda <e235704@ie.u-ryukyu.ac.jp>
2023-12-07 15:29:30 +0900

    tryadd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args){
     String str = null;
-    str.length();
+    try {
+        str.length(); //処理したいコード
+    } catch (NullPointerException e) {
+        System.out.println("NullPointerExceptionが発生しました");//Exceptionが起きたときの処理
+        System.out.println("Exception message: " + e.getMessage());
+    } 
     }
 }
